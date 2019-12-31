0785b2a3a0f90168aa18015f112e84946f95ef5e
Sunvenger <sunvenger@gmail.com>
2019-12-31 18:54:18 +0100

    added user managment web forms base

---
+++ Forms/ManagmentTools/UserManagmentForm.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UserManagmentForm.aspx.cs" Inherits="pozicam_web_forms.Forms.ManagmentTools.UserManagmentForm" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Forms/ManagmentTools/UserManagmentForm.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pozicam_web_forms.Forms.ManagmentTools
+{
+    public partial class UserManagmentForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
