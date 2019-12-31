7abb0f2337b8314a7d07d209413935c19f39e8fe
Sunvenger <sunvenger@gmail.com>
2019-12-31 18:37:58 +0100

    tst from job

--- Appcode/Models/Model1.Designer.cs
+++ Appcode/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Sunvenger\documents\visual studio 2017\Projects\pozicam web forms\pozicam web forms\Appcode\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\skkoryt\Documents\GitHub\pozicam-web-forms\pozicam web forms\Appcode\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
